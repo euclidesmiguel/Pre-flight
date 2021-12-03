0f3171088e92e49b513d9043125bc10638d2ce92
Your Name <you@example.com>
2021-12-03 13:35:02 -0300

    updates help sections in the scripts

--- PreFlight.ps1
+++ PreFlight.ps1
@@ -1,28 +1,47 @@
-﻿#requires -PSEdition "Desktop"
-[CmdletBinding()]
-Param ([switch] $AutoLoad)
-[string] $version = "1.8.5"
+﻿<#
+    .SYNOPSIS
+    Script to perform pre-migration checks in an Exchange Hybrid environment.
 
-<#
-.DESCRIPTION
-###############Disclaimer#####################################################
+    .DESCRIPTION
+    It checks for basic migration pre-requisites, such as:  
 
-This is a sample script. The sample scripts are not supported under any 
-Microsoft standard support program or service. The sample scripts are 
-provided AS IS without warranty of any kind. Microsoft further disclaims all 
-implied warranties including, without limitation, any implied warranties of 
-merchantability or of fitness for a particular purpose. The entire risk 
-arising out of the use or performance of the sample scripts and documentation 
-remains with you. In no event shall Microsoft, its authors, or anyone else 
-involved in the creation, production, or delivery of the scripts be liable 
-for any damages whatsoever (including, without limitation, damages for loss 
-of business profits, business interruption, loss of business information, or 
-other pecuniary loss) arising out of the use of or inability to use the 
-sample scripts or documentation, even if Microsoft has been advised of the 
-possibility of such damages.
+    - Does the target object exist in Office 365?
+    - Does it have the required e-mail addresses?
+    - Are all the e-mail addresses valid?
+    - Does the admin account have the required permissions?
 
-###############Disclaimer#####################################################
+    .PARAMETER Autoload
+    Autoload mail users in the tool.
+
+    .EXAMPLE
+    PS C:\Preflight> .\Preflight.ps1 
+    Runs the script in the local path.
+
+    .NOTES
+    ###############Disclaimer#####################################################
+
+    This is a sample script. The sample scripts are not supported under any 
+    Microsoft standard support program or service. The sample scripts are 
+    provided AS IS without warranty of any kind. Microsoft further disclaims all 
+    implied warranties including, without limitation, any implied warranties of 
+    merchantability or of fitness for a particular purpose. The entire risk 
+    arising out of the use or performance of the sample scripts and documentation 
+    remains with you. In no event shall Microsoft, its authors, or anyone else 
+    involved in the creation, production, or delivery of the scripts be liable 
+    for any damages whatsoever (including, without limitation, damages for loss 
+    of business profits, business interruption, loss of business information, or 
+    other pecuniary loss) arising out of the use of or inability to use the 
+    sample scripts or documentation, even if Microsoft has been advised of the 
+    possibility of such damages.
+
+    ###############Disclaimer#####################################################
 #>
+#requires -PSEdition "Desktop"
+[CmdletBinding()]
+Param (
+    [switch] $AutoLoad
+)
+[string] $version = "1.8.5"
 
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
--- Report.ps1
+++ Report.ps1
@@ -1,29 +1,28 @@
+<#
+    .NOTES
+    ###############Disclaimer#####################################################
+
+    This is a sample script. The sample scripts are not supported under any 
+    Microsoft standard support program or service. The sample scripts are 
+    provided AS IS without warranty of any kind. Microsoft further disclaims all 
+    implied warranties including, without limitation, any implied warranties of 
+    merchantability or of fitness for a particular purpose. The entire risk 
+    arising out of the use or performance of the sample scripts and documentation 
+    remains with you. In no event shall Microsoft, its authors, or anyone else 
+    involved in the creation, production, or delivery of the scripts be liable 
+    for any damages whatsoever (including, without limitation, damages for loss 
+    of business profits, business interruption, loss of business information, or 
+    other pecuniary loss) arising out of the use of or inability to use the 
+    sample scripts or documentation, even if Microsoft has been advised of the 
+    possibility of such damages.
+
+    ###############Disclaimer#####################################################
+#>
 #requires -PSEdition "Desktop"
 [CmdletBinding()]
 param ([PSCredential] $Credential = (New-Object System.Management.Automation.PSCredential ("dummy", (ConvertTo-SecureString "dummy" -AsPlainText -Force))))
 
 [string] $version = "0.9"
-
-<#
-.DESCRIPTION
-###############Disclaimer#####################################################
-
-This is a sample script. The sample scripts are not supported under any 
-Microsoft standard support program or service. The sample scripts are 
-provided AS IS without warranty of any kind. Microsoft further disclaims all 
-implied warranties including, without limitation, any implied warranties of 
-merchantability or of fitness for a particular purpose. The entire risk 
-arising out of the use or performance of the sample scripts and documentation 
-remains with you. In no event shall Microsoft, its authors, or anyone else 
-involved in the creation, production, or delivery of the scripts be liable 
-for any damages whatsoever (including, without limitation, damages for loss 
-of business profits, business interruption, loss of business information, or 
-other pecuniary loss) arising out of the use of or inability to use the 
-sample scripts or documentation, even if Microsoft has been advised of the 
-possibility of such damages.
-
-###############Disclaimer#####################################################
-#>
 
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
